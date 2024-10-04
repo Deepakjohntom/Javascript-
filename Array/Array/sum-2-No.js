776bbd0c39505832e9bae2a67d4e77326dafc27a
D-J-T <91208930+Deepakjohntom@users.noreply.github.com>
2024-10-04 21:53:06 +0530

    Add files via upload

---
+++ Array/Array/sum-2-No.js
@@ -0,0 +1,11 @@
+// let num1 = 10;
+// let num2 = 20;
+// let result = num1 + num2;
+
+// let h2 = document.querySelector("h2");
+// h2.innerText = `The Sum of 2 numberds is ${result}`;
+
+// console.log(`The Sum of 2 numberds is ${result}`);
+let Your_Name = "deepak";
+
+document.querySelector("nameDisplay").innerText = Your_Name;
